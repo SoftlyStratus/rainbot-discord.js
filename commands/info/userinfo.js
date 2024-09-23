9f69bcfe9e52ca8be8c170a0eeffafc421e7518a
SoftlyStratus <zmiles@scalar.cloud>
2024-09-23 01:22:39 -0500

    🎨 refactor: add event and command handler structure for scalable bot
    ✨ Implemented dynamic event handler system to load all events from the 'events' folder.
    ✨ Added dynamic command loading system from the 'commands' folder for modular command management.
    🗃️ Created separate files for handling core events like 'ready' and 'interactionCreate'.
    ⚡ Added sample commands: 'ping' for latency check and 'userinfo' for fetching user information.
    - 🛠️ Improved project structure for better scalability and maintainability.

---
+++ commands/info/userinfo.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { execute } = require('./ping');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('userinfo')
+        .setDescription('Fetches inforomation about the user'),
+    async execute(interaction) {
+        const user = interaction.user;
+        await interaction.reply(`Your username: ${user.username}, Your ID: ${user.id}`);
+
+    },
+};
